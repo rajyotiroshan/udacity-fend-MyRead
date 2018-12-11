3834d0d8c13f2f2ab1179131ef3f80410a8d9530
rajan <rajangreedge@gmail.com>
2018-12-11 19:30:20 +0530

    manageed code with proper Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Main from './components/pages/Main'
+import MainPage from './components/pages/MainPage'
 import SearchPage from './components/pages/Search'
 import {Route} from 'react-router-dom'
 class BooksApp extends React.Component {
@@ -17,7 +17,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component = {Main}/>
+        <Route exact path="/" component = {MainPage}/>
         <Route exact path="/search" component= {SearchPage}/>
       </div>
     )
