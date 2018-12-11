1febcfe08e35fc35a3d54f2253d78583d34f6199
rajan <rajangreedge@gmail.com>
2018-12-11 18:46:50 +0530

    Used Route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 import Main from './components/pages/Main'
 import SearchPage from './components/pages/Search'
+import {Route} from 'react-router-dom'
 class BooksApp extends React.Component {
   state = {
     /**
@@ -16,7 +17,8 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? ( <SearchPage/>) : (<Main/>)}
+        <Route exact path="/" component = {Main}/>
+        <Route exact path="/search" component= {SearchPage}/>
       </div>
     )
   }
