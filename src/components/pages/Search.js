a6ecfdd2d6b0e5aa6b3df3f7cb4225e907be710e
rajan <rajangreedge@gmail.com>
2018-12-12 13:57:27 +0530

    Create searching model

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
 class SearchPage extends Component {
+  state = {
+    searchQuery:'',
+    searchResults:[]
+  }
+
+updateQuery = (query) => {
+  this.setState({searchQuery: query.trim()});
+}
+
+componentDidMount() {
+  //search from the BooksAPI against updated searchQuery
+  BooksAPI.search(this.state.searchQuery).then((resp)=>{
+    this.setState({searchResults:resp});
+  });
+}
+
 	render() {
 		return (
 			<div className="search-books">
@@ -8,20 +25,16 @@ class SearchPage extends Component {
               		<Link className="close-search" 
               				to="/">Close</Link>
               		<div className="search-books-input-wrapper">
-                	{/*
-                  		NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  		You can find these search terms here:
-                  		https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-	
-    		              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            	      you don't find a specific author or title. Every search is limited by search terms.
-                	*/}
-               		 <input type="text" placeholder="Search by title or author"/>
-
-             	 	</div>
+               		   <input type="text" placeholder="Search by title or author" 
+                     onChange={(event)=>this.updateQuery(event.target.value)}/>
+              	 	</div>
             	</div>
             	<div className="search-books-results">
-              		<ol className="books-grid"></ol>
+              		<ol className="books-grid">
+                  {
+                    this.state.searchQuery
+                  }
+                  </ol>
             	</div>
           	</div>
 		)
